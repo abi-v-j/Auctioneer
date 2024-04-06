a985870757960e9cd5b6e694b7af2fc1c138dafb
abi-v-j <abijoy611@gmail.com>
2024-04-07 00:37:47 +0530

    landing page

--- frontend/src/Guest/App.js
+++ frontend/src/Guest/App.js
@@ -1,22 +1,21 @@
 
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import User from './Pages/User';
+import Dealer from './Pages/Dealer';
+import Login from './Pages/Login';
+import VerifyRegistration from './Pages/VerifyRegistration';
+import Main from './Pages/Main';
 
-
-
-// import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import User from './Pages/User';
-// import Dealer from './Pages/Dealer';
-// import Login from './Pages/Login';
-// import VerifyRegistration from './Pages/VerifyRegistration';
-
-// const App = () => {
-//     return (
-//          <Routes>
-<Route path='/UserRegistration' element={<User />} />
-<Route path='/DealerRegistration' element={<Dealer />} />
-<Route path='/' element={<Login />} />
-<Route path='/VerifyRegistration' element={<VerifyRegistration />} />
-</Routes>
-//     )
-// }
-// export default App
+const App = () => {
+    return (
+        <Routes>
+            <Route path='/UserRegistration' element={<User />} />
+            <Route path='/DealerRegistration' element={<Dealer />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/VerifyRegistration' element={<VerifyRegistration />} />
+            <Route path='/' element={< Main/>} />
+        </Routes>
+    )
+}
+export default App
