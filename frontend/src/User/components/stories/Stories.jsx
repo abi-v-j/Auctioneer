95b4f0b45cbeb3099b8d5d85e314127dee89e722
abi-v-j <abijoy611@gmail.com>
2024-04-15 01:29:10 +0530

    completed

--- frontend/src/User/components/stories/Stories.jsx
+++ frontend/src/User/components/stories/Stories.jsx
@@ -1,30 +1,20 @@
-import './stories.scss'
-import { Box, Card } from '@mui/material'
+import "./stories.scss";
+import { Box, Card } from "@mui/material";
 
-const Stories = ({rows}) => {
-  
-  
+const Stories = ({ rows }) => {
+  return (
+    <div className="stories">
+      {rows.map((story, key) => (
+        <Box sx={{ width: 250 }} key={key}>
+          <Card className="story" key={key}>
+            <img src={story.galleries[0].lotImgsrc} alt="" />
+            <span>{story.auctionheadToken}</span>
+            <div className="link"></div>
+          </Card>
+        </Box>
+      ))}
+    </div>
+  );
+};
 
-   return (
-      <div className='stories'>
-         {rows.map((story, key) => (
-            <Box sx={{width:250}} key={key}>
-               <Card
-                  className='story'
-                  key={key}
-               >
-                  <img
-                     src={story.galleries[0].lotImgsrc}
-                     alt=''
-                  />
-                  <span>{story.auctionheadToken}</span>
-                  <a className='link'  rel='noopener noreferrer'></a>
-
-               </Card>
-            </Box>
-         ))}
-      </div>
-   )
-}
-
-export default Stories
+export default Stories;
